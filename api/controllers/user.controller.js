7ae62bc03ff9bb164bbc27a0030d0a4311ac5c32
Ritwik Mukherjee <mritwik369@gmail.com>
2023-01-29 18:04:02 +0530

    fix: error message updated

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,7 +6,7 @@ module.exports.createUser = async (req, res) => {
     req.body.user_type = req.params.userType;
     const existingUser = await User.findOne({ phone_number: req.body.phone_number });
     if (existingUser) return res.status(400).json({
-        err: "NOT able to save user in DB"
+        err: "User already exists"
     });
     const user = new User(req.body);
     user.save((err, user) => {
