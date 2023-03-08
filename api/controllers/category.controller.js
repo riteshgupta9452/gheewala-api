33d7414a73f84924ba8a3327be66d67148e63902
ritesh <riteshgupta9452@gmail.com>
2023-03-08 23:32:25 +0530

    category api

---
+++ api/controllers/category.controller.js
@@ -0,0 +1,22 @@
+const ProductCategory = require("./../models/product-category");
+const ObjectId = require("mongoose").Types.ObjectId;
+
+module.exports.getCategories = async (req, res) => {
+  const categories = await ProductCategory.find({});
+  res.json({ status: true, data: categories });
+};
+
+module.exports.toggleCategory = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    _id: ObjectId(req.params.id),
+  });
+  if (!category)
+    return res.status(404).json({
+      err: "Category not found",
+    });
+  category.is_viewable = !category.is_viewable;
+  category.save();
+  return res
+    .status(200)
+    .json({ status: true, message: "Status successfully updated" });
+};
--- api/routes/admin.routes.js
+++ api/routes/admin.routes.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require('../controllers/user.controller');
-const adminController = require('../controllers/admin.controller');
-const middleware = require('../functions/middleware');
+const userController = require("../controllers/user.controller");
+const adminController = require("../controllers/admin.controller");
+const categoryController = require("./../controllers/category.controller");
+const middleware = require("../functions/middleware");
 
-router.post('/generate-password', adminController.generatePassword);
-router.post('/login', adminController.login);
+router.post("/generate-password", adminController.generatePassword);
+router.post("/login", adminController.login);
 
 router.use(middleware.tokenVerify, (req, res, next) => {
-    req.admin_id = req.userId;
-    next();
+  req.admin_id = req.userId;
+  next();
 });
 
-router.get('/users', userController.getAllUsers);
+router.get("/users", userController.getAllUsers);
 
-router.get('/user/:user_id', (req, res, next) => {
+router.get(
+  "/user/:user_id",
+  (req, res, next) => {
     req.userId = req.params.user_id;
     next();
-}, userController.getUser);
+  },
+  userController.getUser
+);
 
-router.get('toggle-verification/:user_id', userController.toggleVerification);
+router.get("/toggle-verification/:user_id", userController.toggleVerification);
+router.get("/categories", categoryController.getCategories);
+router.get("/category/:id", categoryController.toggleCategory);
 
 module.exports = router;
